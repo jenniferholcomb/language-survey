2f5520429817713bde42321e1cc768d471bb565c
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-10-21 15:17:28 -0700

    Troubleshooting addEventListener, added text to README.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
 // Business Logic
 
 // User Interface Logic
-function handleSelect(event) {
-  event.preventDefault();
+function getAndSetSelection () {
   const selection1 = parseInt(document.getElementById("answer1").value);
   const selection2 = parseInt(document.getElementById("answer2").value);
   const selection3 = parseInt(document.getElementById("answer3").value);
   const selection4 = parseInt(document.getElementById("answer4").value);
   const selection5 = parseInt(document.getElementById("answer5").value);
-  const answerValue = selection1 + selection2 + selection3 + selection4 + selection5;
 
   let jsTally = 0;
   let cSharpTally = 0;
@@ -55,20 +53,28 @@ function handleSelect(event) {
     }
   } else if (cSharpTally === jsTally) {
     document.getElementById("javaScript").removeAttribute("class");
-    document.getElementById("cShartTally").removeAttribute("class");
+    document.getElementById("cSharp").removeAttribute("class");
   } else if (cSharpTally === swiftTally) {
     document.getElementById("javaScript").removeAttribute("class");
-    document.getElementById("cShartTally").removeAttribute("class");
+    document.getElementById("cSharp").removeAttribute("class");
   } else if (cSharpTally > swiftTally) {
-    document.getElementById("cShartTally").removeAttribute("class");
+    document.getElementById("cSharp").removeAttribute("class");
   } else if (swiftTally > cSharpTally) {
-    document.getElementById("swiftTally").removeAttribute("class");
+    document.getElementById("swift").removeAttribute("class");
   } else {
     document.getElementById("none").removeAttribute("class");
   }
-  
 }
 
-window.addEventListener("load", function() {
-  document.getElementById("select-form").addEventListener("submit", handleSelect);
+function handleSelect(event) {
+  let form = document.querySelector("form");
+  form.onsubmit = function(event) {
+    event.preventDefault();
+    getAndSetSelection();
+  }
+}
+
+window.addEventListener("load", handleSelect) {
+  const form = document.getElementById("form");
+  form.addEventListener("")
 });
